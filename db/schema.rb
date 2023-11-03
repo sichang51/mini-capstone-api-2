837dd2b638a2927da231a013c781ffdba0d139ee
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-03 16:41:52 -0700

    added supplier id to products since the change didn't work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_03_233628) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_03_234059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_03_233628) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "inventory"
+    t.integer "supplier_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
