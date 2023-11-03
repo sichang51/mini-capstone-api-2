0bc4293521ef529cb03159d304fdb1edf2924003
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-03 16:27:29 -0700

    added migration for price to cast and decimals

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,32 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get "/all_products", controller: "products", action: "all_products"
+
+  # get "/first_product", controller: "products", action: "first_product"
+
+  # get "/second_product", controller: "products", action: "second_product"
+
+  # get "/one_product/:id" => "products#one_product"
+
+  get "/products" => "products#index"
+
+  get "/products/:id" => "products#show"
+
+  post "/products.json" => "products#create"
+
+  patch "/products/:id" => "products#update"
+
+  delete "/products/:id" => "products#destroy"
+
+  get "/suppliers" => "suppliers#index"
+
+  get "/suppliers/:id" => "suppliers#show"
+
+  post "/suppliers.json" => "suppliers#create"
+
+  patch "/suppliers/:id" => "suppliers#update"
+
+  delete "/suppliers/:id" => "suppliers#destroy"
 end
---
+++ db/migrate/20231103232643_price_to_products.rb
@@ -0,0 +1,6 @@
+class PriceToProducts < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :price, "numeric USING CAST(price AS numeric)"
+    change_column :products, :price, :decimal, precision: 9, scale: 2
+  end
+end
